120361c2518db89f7856ab6f2a7a63492d8b924b
Mr.L <admin@duxphp.com>
2019-03-28 11:36:37 +0800

    no message

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-if (!defined('ROOT_PATH')) define('ROOT_PATH', str_replace('\\', '/', __DIR__) . '/');
+if (!defined('ROOT_PATH')) {
+    define('ROOT_PATH', str_replace('\\', '/', __DIR__) . '/');
+}
 require __DIR__ . '/vendor/autoload.php';
+if (!is_dir(__DIR__ . '/vendor/')) {
+    exit('Composer not installed');
+}
+if (version_compare(PHP_VERSION, '7.1', '<')) {
+    exit('PHP version cannot be less than 7.1');
+}
 \dux\Start::run();
-
