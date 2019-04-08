2d2355136fe88c6b2d3e2f83bae29fe0930d9267
Mr.L <admin@duxphp.com>
2019-04-08 09:50:25 +0800

    no message

--- app/wechat/api/MiniappApi.php
+++ app/wechat/api/MiniappApi.php
@@ -13,6 +13,9 @@ class MiniappApi extends BaseApi {
     protected $_middle = 'wechat/Miniapp';
 
     public function login() {
+        if(empty($this->data['iv']) || empty($this->data['encryptedData'])) {
+            $this->error('请进行用户授权');
+        }
         target($this->_middle, 'middle')->setParams([
             'app' => $this->apiApp
         ])->getUserInfo($this->data['code'], $this->data['iv'], $this->data['encryptedData'])->export(function ($return) {
