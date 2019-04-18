3507b47971b3c89b6269aa3b41eae4b3181f6bf9
Mr.L <admin@duxphp.com>
2019-04-18 11:02:01 +0800

    no message

--- app/install/data/install.sql
+++ app/install/data/install.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.25)
 # Database: duxcms3
-# Generation Time: 2019-03-27 09:06:53 +0000
+# Generation Time: 2019-04-18 03:00:46 +0000
 # ************************************************************
 
 
@@ -30,7 +30,6 @@ CREATE TABLE `dux_article` (
   `article_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `class_id` int(10) NOT NULL DEFAULT '0' COMMENT '栏目ID',
   `title` varchar(250) DEFAULT '',
-  `sub_title` varchar(250) DEFAULT '',
   `keyword` varchar(250) DEFAULT '',
   `description` varchar(250) DEFAULT '',
   `image` varchar(250) DEFAULT '',
@@ -50,9 +49,9 @@ CREATE TABLE `dux_article` (
 LOCK TABLES `dux_article` WRITE;
 /*!40000 ALTER TABLE `dux_article` DISABLE KEYS */;
 
-INSERT INTO `dux_article` (`article_id`, `class_id`, `title`, `sub_title`, `keyword`, `description`, `image`, `auth`, `content`, `tags_id`, `create_time`, `update_time`, `virtual_view`, `view`, `status`, `sort`)
+INSERT INTO `dux_article` (`article_id`, `class_id`, `title`, `keyword`, `description`, `image`, `auth`, `content`, `tags_id`, `create_time`, `update_time`, `virtual_view`, `view`, `status`, `sort`)
 VALUES
-	(1,1,'这是一篇默认的文章','','','欢迎使用DuxCMS作为你的文章管理系统，请开始编写第一篇内容吧...','/theme/default/images/show1.webp','','&lt;p&gt;欢迎使用DuxCMS作为你的文章管理系统，请开始编写第一篇内容吧&lt;/p&gt;','',1553588391,0,0,45,1,0);
+	(1,1,'这是一篇默认的文章','','欢迎使用DuxCMS作为你的文章管理系统，请开始编写第一篇内容吧...','/theme/default/images/show1.webp','','&lt;p&gt;欢迎使用DuxCMS作为你的文章管理系统，请开始编写第一篇内容吧&lt;/p&gt;','',1553588391,0,0,45,1,0);
 
 /*!40000 ALTER TABLE `dux_article` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -156,7 +155,8 @@ VALUES
 	(31,'site_wap','https://shop.zjjxzl.cn',''),
 	(32,'info_logo','',''),
 	(33,'style_login_img','http://lib.a.cuhuibao.com/duxup_e0a12d20015e90a1edcae0c15639df67.png',''),
-	(34,'page_theme','default','');
+	(34,'page_theme','default',''),
+	(36,'tools_apis','docs','');
 
 /*!40000 ALTER TABLE `dux_site_config` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -397,7 +397,7 @@ LOCK TABLES `dux_system_role` WRITE;
 
 INSERT INTO `dux_system_role` (`role_id`, `name`, `description`, `purview`)
 VALUES
-	(1,'管理员','系统后台管理员','a:93:{i:0;s:21:\"article.Content.index\";i:1;s:19:\"article.Content.add\";i:2;s:20:\"article.Content.edit\";i:3;s:22:\"article.Content.status\";i:4;s:19:\"article.Content.del\";i:5;s:19:\"article.Class.index\";i:6;s:17:\"article.Class.add\";i:7;s:18:\"article.Class.edit\";i:8;s:20:\"article.Class.status\";i:9;s:17:\"article.Class.del\";i:10;s:15:\"page.Page.index\";i:11;s:13:\"page.Page.add\";i:12;s:14:\"page.Page.edit\";i:13;s:16:\"page.Page.status\";i:14;s:13:\"page.Page.del\";i:15;s:17:\"site.Config.index\";i:16;s:15:\"site.Config.tpl\";i:17;s:17:\"site.Search.index\";i:18;s:15:\"site.Search.add\";i:19;s:16:\"site.Search.edit\";i:20;s:15:\"site.Search.del\";i:21;s:19:\"site.Fragment.index\";i:22;s:17:\"site.Fragment.add\";i:23;s:18:\"site.Fragment.edit\";i:24;s:20:\"site.Fragment.status\";i:25;s:17:\"site.Fragment.del\";i:26;s:14:\"site.Diy.index\";i:27;s:12:\"site.Diy.add\";i:28;s:13:\"site.Diy.edit\";i:29;s:15:\"site.Diy.status\";i:30;s:12:\"site.Diy.del\";i:31;s:18:\"site.DiyData.index\";i:32;s:16:\"site.DiyData.add\";i:33;s:17:\"site.DiyData.edit\";i:34;s:19:\"site.DiyData.status\";i:35;s:16:\"site.DiyData.del\";i:36;s:22:\"statis.SiteViews.index\";i:37;s:18:\"system.Index.index\";i:38;s:21:\"system.Index.userData\";i:39;s:19:\"system.Notice.index\";i:40;s:17:\"system.Notice.del\";i:41;s:19:\"system.Update.index\";i:42;s:19:\"system.Config.index\";i:43;s:18:\"system.Config.user\";i:44;s:18:\"system.Config.info\";i:45;s:20:\"system.Config.upload\";i:46;s:25:\"system.ConfigManage.index\";i:47;s:23:\"system.ConfigManage.add\";i:48;s:24:\"system.ConfigManage.edit\";i:49;s:26:\"system.ConfigManage.status\";i:50;s:23:\"system.ConfigManage.del\";i:51;s:22:\"system.ConfigApi.index\";i:52;s:20:\"system.ConfigApi.add\";i:53;s:21:\"system.ConfigApi.edit\";i:54;s:23:\"system.ConfigApi.status\";i:55;s:20:\"system.ConfigApi.del\";i:56;s:25:\"system.ConfigUpload.index\";i:57;s:24:\"system.ConfigUpload.edit\";i:58;s:17:\"system.User.index\";i:59;s:15:\"system.User.add\";i:60;s:16:\"system.User.edit\";i:61;s:18:\"system.User.status\";i:62;s:15:\"system.User.del\";i:63;s:17:\"system.Role.index\";i:64;s:15:\"system.Role.add\";i:65;s:16:\"system.Role.edit\";i:66;s:15:\"system.Role.del\";i:67;s:18:\"system.Debug.index\";i:68;s:16:\"system.Debug.del\";i:69;s:22:\"system.SystemLog.index\";i:70;s:20:\"system.SystemLog.del\";i:71;s:24:\"system.Application.index\";i:72;s:22:\"system.Application.add\";i:73;s:23:\"system.Application.edit\";i:74;s:22:\"system.Application.del\";i:75;s:20:\"tools.SendData.index\";i:76;s:16:\"tools.Send.index\";i:77;s:14:\"tools.Send.add\";i:78;s:15:\"tools.Send.info\";i:79;s:20:\"tools.SendConf.index\";i:80;s:22:\"tools.SendConf.setting\";i:81;s:19:\"tools.SendTpl.index\";i:82;s:17:\"tools.SendTpl.add\";i:83;s:18:\"tools.SendTpl.edit\";i:84;s:17:\"tools.SendTpl.del\";i:85;s:23:\"tools.SendDefault.index\";i:86;s:17:\"tools.Label.index\";i:87;s:17:\"tools.Queue.index\";i:88;s:21:\"tools.QueueConf.index\";i:89;s:25:\"wechat.WechatConfig.index\";i:90;s:23:\"wechat.MenuConfig.index\";i:91;s:26:\"wechat.MiniappConfig.index\";i:92;s:22:\"wechat.AppConfig.index\";}');
+	(1,'管理员','系统后台管理员','a:95:{i:0;s:21:\"article.Content.index\";i:1;s:19:\"article.Content.add\";i:2;s:20:\"article.Content.edit\";i:3;s:22:\"article.Content.status\";i:4;s:19:\"article.Content.del\";i:5;s:19:\"article.Class.index\";i:6;s:17:\"article.Class.add\";i:7;s:18:\"article.Class.edit\";i:8;s:20:\"article.Class.status\";i:9;s:17:\"article.Class.del\";i:10;s:15:\"page.Page.index\";i:11;s:13:\"page.Page.add\";i:12;s:14:\"page.Page.edit\";i:13;s:16:\"page.Page.status\";i:14;s:13:\"page.Page.del\";i:15;s:17:\"site.Config.index\";i:16;s:15:\"site.Config.tpl\";i:17;s:17:\"site.Search.index\";i:18;s:15:\"site.Search.add\";i:19;s:16:\"site.Search.edit\";i:20;s:15:\"site.Search.del\";i:21;s:19:\"site.Fragment.index\";i:22;s:17:\"site.Fragment.add\";i:23;s:18:\"site.Fragment.edit\";i:24;s:20:\"site.Fragment.status\";i:25;s:17:\"site.Fragment.del\";i:26;s:14:\"site.Diy.index\";i:27;s:12:\"site.Diy.add\";i:28;s:13:\"site.Diy.edit\";i:29;s:15:\"site.Diy.status\";i:30;s:12:\"site.Diy.del\";i:31;s:18:\"site.DiyData.index\";i:32;s:16:\"site.DiyData.add\";i:33;s:17:\"site.DiyData.edit\";i:34;s:19:\"site.DiyData.status\";i:35;s:16:\"site.DiyData.del\";i:36;s:22:\"statis.SiteViews.index\";i:37;s:18:\"system.Index.index\";i:38;s:21:\"system.Index.userData\";i:39;s:19:\"system.Notice.index\";i:40;s:17:\"system.Notice.del\";i:41;s:19:\"system.Update.index\";i:42;s:19:\"system.Config.index\";i:43;s:18:\"system.Config.user\";i:44;s:18:\"system.Config.info\";i:45;s:20:\"system.Config.upload\";i:46;s:25:\"system.ConfigManage.index\";i:47;s:23:\"system.ConfigManage.add\";i:48;s:24:\"system.ConfigManage.edit\";i:49;s:26:\"system.ConfigManage.status\";i:50;s:23:\"system.ConfigManage.del\";i:51;s:22:\"system.ConfigApi.index\";i:52;s:20:\"system.ConfigApi.add\";i:53;s:21:\"system.ConfigApi.edit\";i:54;s:23:\"system.ConfigApi.status\";i:55;s:20:\"system.ConfigApi.del\";i:56;s:25:\"system.ConfigUpload.index\";i:57;s:24:\"system.ConfigUpload.edit\";i:58;s:17:\"system.User.index\";i:59;s:15:\"system.User.add\";i:60;s:16:\"system.User.edit\";i:61;s:18:\"system.User.status\";i:62;s:15:\"system.User.del\";i:63;s:17:\"system.Role.index\";i:64;s:15:\"system.Role.add\";i:65;s:16:\"system.Role.edit\";i:66;s:15:\"system.Role.del\";i:67;s:18:\"system.Debug.index\";i:68;s:16:\"system.Debug.del\";i:69;s:22:\"system.SystemLog.index\";i:70;s:20:\"system.SystemLog.del\";i:71;s:24:\"system.Application.index\";i:72;s:22:\"system.Application.add\";i:73;s:23:\"system.Application.edit\";i:74;s:22:\"system.Application.del\";i:75;s:20:\"tools.SendData.index\";i:76;s:16:\"tools.Send.index\";i:77;s:14:\"tools.Send.add\";i:78;s:15:\"tools.Send.info\";i:79;s:20:\"tools.SendConf.index\";i:80;s:22:\"tools.SendConf.setting\";i:81;s:19:\"tools.SendTpl.index\";i:82;s:17:\"tools.SendTpl.add\";i:83;s:18:\"tools.SendTpl.edit\";i:84;s:17:\"tools.SendTpl.del\";i:85;s:23:\"tools.SendDefault.index\";i:86;s:17:\"tools.Label.index\";i:87;s:17:\"tools.Queue.index\";i:88;s:21:\"tools.QueueConf.index\";i:89;s:15:\"tools.Api.index\";i:90;s:14:\"tools.Api.make\";i:91;s:25:\"wechat.WechatConfig.index\";i:92;s:23:\"wechat.MenuConfig.index\";i:93;s:26:\"wechat.MiniappConfig.index\";i:94;s:22:\"wechat.AppConfig.index\";}');
 
 /*!40000 ALTER TABLE `dux_system_role` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -445,7 +445,7 @@ LOCK TABLES `dux_system_user` WRITE;
 
 INSERT INTO `dux_system_user` (`user_id`, `role_id`, `nickname`, `username`, `password`, `avatar`, `status`, `reg_time`, `login_time`, `login_ip`, `role_ext`)
 VALUES
-	(1,1,'admin','admin','21232f297a57a5a743894a0e4a801fc3','',1,0,0,'127.0.0.1','');
+	(1,1,'admin','admin','21232f297a57a5a743894a0e4a801fc3','',1,0,1555299701,'127.0.0.1','');
 
 /*!40000 ALTER TABLE `dux_system_user` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -544,25 +544,6 @@ CREATE TABLE `dux_tools_send_config` (
 
 
 
-# Dump of table dux_tools_send_data
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `dux_tools_send_data`;
-
-CREATE TABLE `dux_tools_send_data` (
-  `data_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type` varchar(50) DEFAULT '',
-  `label` varchar(250) DEFAULT '',
-  `class` varchar(250) DEFAULT '',
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `title` varchar(250) DEFAULT '',
-  `data` text,
-  `url` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`data_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
 # Dump of table dux_tools_send_default
 # ------------------------------------------------------------
 
@@ -604,97 +585,6 @@ CREATE TABLE `dux_tools_send_tpl` (
   `content` text COMMENT '模板内容',
   `time` int(10) NOT NULL DEFAULT '0' COMMENT '时间',
   PRIMARY KEY (`tpl_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-# Dump of table dux_wechat_app
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `dux_wechat_app`;
-
-CREATE TABLE `dux_wechat_app` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(250) DEFAULT '',
-  `label` varchar(20) DEFAULT '',
-  `appid` varchar(100) NOT NULL DEFAULT '',
-  `secret` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-# Dump of table dux_wechat_config
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `dux_wechat_config`;
-
-CREATE TABLE `dux_wechat_config` (
-  `config_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(250) DEFAULT '',
-  `content` text,
-  `description` varchar(250) DEFAULT '',
-  PRIMARY KEY (`config_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-LOCK TABLES `dux_wechat_config` WRITE;
-/*!40000 ALTER TABLE `dux_wechat_config` DISABLE KEYS */;
-
-INSERT INTO `dux_wechat_config` (`config_id`, `name`, `content`, `description`)
-VALUES
-	(1,'appid','','AppID'),
-	(2,'secret','','AppSecret'),
-	(3,'token','','Token'),
-	(4,'aeskey','','EncodingAESKey'),
-	(7,'message_focus','欢迎关注DuxCMS',''),
-	(8,'message_name','DuxCMS',''),
-	(10,'mp_name','DuxCMS',''),
-	(11,'mp_desc','点击保存到相册可以识别二维码,复制公众号可以到微信进行搜索',''),
-	(12,'mp_qrcode','public/images/logo.png','');
-
-/*!40000 ALTER TABLE `dux_wechat_config` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Dump of table dux_wechat_menu
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `dux_wechat_menu`;
-
-CREATE TABLE `dux_wechat_menu` (
-  `menu_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `parent_id` int(10) NOT NULL DEFAULT '0',
-  `name` varchar(100) DEFAULT '',
-  `type` tinyint(1) NOT NULL,
-  `sort` int(10) NOT NULL DEFAULT '0',
-  `data` text,
-  PRIMARY KEY (`menu_id`),
-  KEY `parent_id` (`parent_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-LOCK TABLES `dux_wechat_menu` WRITE;
-/*!40000 ALTER TABLE `dux_wechat_menu` DISABLE KEYS */;
-
-INSERT INTO `dux_wechat_menu` (`menu_id`, `parent_id`, `name`, `type`, `sort`, `data`)
-VALUES
-	(13,0,'默认菜单',2,0,'{\"type\":\"view\",\"url\":\"http:\\/\\/www.duxphp.com\"}');
-
-/*!40000 ALTER TABLE `dux_wechat_menu` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Dump of table dux_wechat_miniapp
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `dux_wechat_miniapp`;
-
-CREATE TABLE `dux_wechat_miniapp` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(250) DEFAULT '',
-  `label` varchar(20) DEFAULT '',
-  `appid` varchar(100) NOT NULL DEFAULT '',
-  `secret` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
